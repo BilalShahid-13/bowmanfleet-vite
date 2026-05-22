f048dd5219cc3955aadd6e6507420e820a287963
Bilal Shahid <bcsm-f21-249@superior.edu.pk>
2026-05-22 20:28:24 +0500

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
-import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-import createHtaccessPlugin from 'vite-plugin-htaccess';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +12,7 @@ export default defineConfig({
     TanStackRouterVite(),
     react(),
     tailwindcss(),
-    createHtaccessPlugin(),
+    // createHtaccessPlugin(),
     babel({ presets: [reactCompilerPreset()] })
 
   ],
